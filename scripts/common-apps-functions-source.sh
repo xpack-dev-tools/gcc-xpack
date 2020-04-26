739f7ee877862bfa37c8000fbad75f871f2dad19
Liviu Ionescu <ilg@livius.net>
2020-04-26 13:59:39 +0300

    common-apps: add --enable-libmpx

--- scripts/common-apps-functions-source.sh
+++ scripts/common-apps-functions-source.sh
@@ -332,6 +332,7 @@ function do_gcc()
           config_options+=("--enable-cloog-backend=isl")
           config_options+=("--enable-libgomp")
           config_options+=("--enable-libssp")
+          config_options+=("--enable-libmpx")
          
           config_options+=("--enable-libatomic")
           config_options+=("--enable-graphite")
