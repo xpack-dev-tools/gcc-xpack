066beb1480397bad35571de363026043fdaea564
Liviu Ionescu <ilg@livius.net>
2020-04-26 09:41:02 +0300

    tests/common: add hello gcc test

--- tests/scripts/common-functions-source.sh
+++ tests/scripts/common-functions-source.sh
@@ -11,17 +11,50 @@
 # Common functions used in various tests.
 #
 # Requires 
-# - app_absolute_folder_path
-# - test_absolute_folder_path
+# - app_folder_path
+# - test_folder_path
 # - archive_platform (win32|linux|darwin)
 
 # -----------------------------------------------------------------------------
 
 function run_tests()
 {
-  run_app "${app_absolute_folder_path}/bin/g++" --version
+  echo
+  echo "Testing if gcc starts properly..."
 
-  # TODO: add more, if possible.
+  run_app "${app_folder_path}/bin/gcc" --version
+  run_app "${app_folder_path}/bin/g++" --version
+
+  echo
+  echo "Testing if gcc compiles simple Hello programs..."
+
+  local tmp="$(mktemp)"
+  rm -rf "${tmp}"
+
+  mkdir -p "${tmp}"
+  cd "${tmp}"
+
+  # Note: __EOF__ is quoted to prevent substitutions here.
+  cat <<'__EOF__' > hello.c
+#include <stdio.h>
+
+int
+main(int argc, char* argv[])
+{
+  printf("Hello\n");
+}
+__EOF__
+
+  # Test C compile and link in a single step.
+  run_app "${APP_PREFIX}/bin/gcc" -o hello-c1 hello.c
+  show_libs hello-c1
+
+  if [ "x$(./hello-c1)x" == "xHellox" ]
+  then
+    echo "hello-c1 ok"
+  else
+    exit 1
+  fi
 }
 
 # -----------------------------------------------------------------------------
