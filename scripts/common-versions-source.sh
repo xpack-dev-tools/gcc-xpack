f19ce84afc62a569d3cbcdc80b23fa1fb0fef3ac
Liviu Ionescu <ilg@livius.net>
2020-04-28 18:42:09 +0300

    common-versions use uppercase MinGW

--- scripts/common-versions-source.sh
+++ scripts/common-versions-source.sh
@@ -21,8 +21,8 @@ function do_build_versions()
 
   if [ "${TARGET_PLATFORM}" == "win32" ]
   then
-    GCC_BRANDING="${BRANDING_PREFIX} Mingw-w64 ${APP_NAME}\x2C ${TARGET_BITS}-bit"
-    BINUTILS_BRANDING="${BRANDING_PREFIX} Mingw-w64 binutils\x2C ${TARGET_BITS}-bit"
+    GCC_BRANDING="${BRANDING_PREFIX} MinGW-w64 ${APP_NAME}\x2C ${TARGET_BITS}-bit"
+    BINUTILS_BRANDING="${BRANDING_PREFIX} MinGW-w64 binutils\x2C ${TARGET_BITS}-bit"
   else
     GCC_BRANDING="${BRANDING_PREFIX} ${APP_NAME}\x2C ${TARGET_BITS}-bit"
     BINUTILS_BRANDING="${BRANDING_PREFIX} binutils\x2C ${TARGET_BITS}-bit"
