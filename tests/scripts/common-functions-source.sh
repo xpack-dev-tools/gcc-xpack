1bc21e28cb67402879baa78a5155c99edc8fe555
Liviu Ionescu <ilg@livius.net>
2021-05-17 15:29:41 +0300

    tests/common: remove binutils tests

--- tests/scripts/common-functions-source.sh
+++ tests/scripts/common-functions-source.sh
@@ -25,9 +25,10 @@ function run_tests()
   run_app "${app_folder_path}/bin/gcc" --version
   run_app "${app_folder_path}/bin/g++" --version
 
-  run_app "${app_folder_path}/bin/gcc-ar" --version
-  run_app "${app_folder_path}/bin/gcc-nm" --version
-  run_app "${app_folder_path}/bin/gcc-ranlib" --version
+  # run_app "${app_folder_path}/bin/gcc-ar" --version
+  # run_app "${app_folder_path}/bin/gcc-nm" --version
+  # run_app "${app_folder_path}/bin/gcc-ranlib" --version
+
   run_app "${app_folder_path}/bin/gcov" --version
   run_app "${app_folder_path}/bin/gcov-dump" --version
   run_app "${app_folder_path}/bin/gcov-tool" --version
@@ -233,22 +234,6 @@ __EOF__
 
 
   # TODO: test creating libraries, static and shared.
-
-  # ---------------------------------------------------------------------------
-
-  echo
-  echo "Testing if binutils start properly..."
-
-  run_app "${app_folder_path}/bin/ar" --version
-  run_app "${app_folder_path}/bin/as" --version
-  run_app "${app_folder_path}/bin/ld" --version
-  run_app "${app_folder_path}/bin/nm" --version
-  run_app "${app_folder_path}/bin/objcopy" --version
-  run_app "${app_folder_path}/bin/objdump" --version
-  run_app "${app_folder_path}/bin/ranlib" --version
-  run_app "${app_folder_path}/bin/size" --version
-  run_app "${app_folder_path}/bin/strings" --version
-  run_app "${app_folder_path}/bin/strip" --version
 
 }
 
