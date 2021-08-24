f1e96cf414de570a30cd8852a9e34387d25d1980
Liviu Ionescu <ilg@livius.net>
2021-08-24 23:05:15 +0300

    common-apps: fix missing then

--- scripts/common-apps-functions-source.sh
+++ scripts/common-apps-functions-source.sh
@@ -1148,6 +1148,7 @@ function test_gcc_one()
           # "lock.c: LOCKTABLEENTRY.crit" wait timed out in thread 0062, blocked by 0063, retrying (60 sec)
           echo "The test ${prefix}tlstest-main${suffix} is known to hang on wine; ignored."
         elif [ "$(uname)" == "Msys" -a "${prefix}" == "static-" ]
+        then
           echo "The test ${prefix}tlstest-main${suffix} is known to hang on GA; ignored."
         else
           run_app ./${prefix}tlstest-main${suffix} || echo "The test ${prefix}tlstest-main${suffix} is known to fail; ignored."
