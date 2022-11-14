fdd2ee407fd21bffe9ac6e7d12f301472176f918
Liviu Ionescu <ilg@livius.net>
2022-11-14 18:43:22 +0200

    gdb.sh: xbb_activate_dependencies_dev

--- scripts/dependencies/gdb.sh
+++ scripts/dependencies/gdb.sh
@@ -58,7 +58,7 @@ function build_gdb()
       mkdir -pv "${XBB_BUILD_FOLDER_PATH}/${gdb_folder_name}"
       cd "${XBB_BUILD_FOLDER_PATH}/${gdb_folder_name}"
 
-      xbb_activate_installed_dev
+      xbb_activate_dependencies_dev
 
       CPPFLAGS="${XBB_CPPFLAGS}"
       CFLAGS="${XBB_CFLAGS_NO_W}"
