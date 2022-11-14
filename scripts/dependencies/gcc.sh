06d2abd717a1efa49c9951a140f31cb38a611cb1
Liviu Ionescu <ilg@livius.net>
2022-11-14 18:42:32 +0200

    gcc.sh: add xbb_adjust_ldflags_rpath

--- scripts/dependencies/gcc.sh
+++ scripts/dependencies/gcc.sh
@@ -165,6 +165,7 @@ function build_gcc()
         CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
         # LDFLAGS="${XBB_LDFLAGS_APP_STATIC_GCC}"
         LDFLAGS="${XBB_LDFLAGS_APP}"
+        xbb_adjust_ldflags_rpath
 
         if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
         then
