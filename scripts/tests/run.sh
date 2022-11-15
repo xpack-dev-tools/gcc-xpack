f8f7d72e98ca4b205c6189e01496443348b85b3a
Liviu Ionescu <ilg@livius.net>
2022-11-15 23:06:53 +0200

    tests/run.sh: cleanups

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -19,11 +19,8 @@
 
 function tests_run_all()
 {
-  XBB_GCC_VERSION="$(echo "${XBB_RELEASE_VERSION}" | sed -e 's|-.*||')"
-  XBB_GCC_VERSION_MAJOR=$(echo ${XBB_GCC_VERSION} | sed -e 's|\([0-9][0-9]*\)\..*|\1|')
-
-  echo
-  env | sort
+  # XBB_GCC_VERSION="$(echo "${XBB_RELEASE_VERSION}" | sed -e 's|-.*||')"
+  # XBB_GCC_VERSION_MAJOR=$(echo ${XBB_GCC_VERSION} | sed -e 's|\([0-9][0-9]*\)\..*|\1|')
 
   # Call the functions defined in the build code.
   if [ "${XBB_HOST_PLATFORM}" != "darwin" ]
@@ -37,53 +34,6 @@ function tests_run_all()
   then
     test_gdb
   fi
-}
-
-# -----------------------------------------------------------------------------
-
-function tests_update_system()
-{
-  local image_name="$1"
-
-  # Make sure that the minimum prerequisites are met.
-  if [[ ${image_name} == github-actions-ubuntu* ]]
-  then
-    : # sudo apt-get -qq install -y XXX
-  elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
-  then
-    run_verbose apt-get -qq update
-    run_verbose apt-get -qq install -y git-core curl tar gzip lsb-release binutils
-    run_verbose apt-get -qq install -y libc6-dev libstdc++6 # TODO: get rid of them
-  elif [[ ${image_name} == *centos* ]] || [[ ${image_name} == *redhat* ]] || [[ ${image_name} == *fedora* ]]
-  then
-    run_verbose yum install -y -q git curl tar gzip redhat-lsb-core binutils which
-    run_verbose yum install -y -q glibc-devel libstdc++-devel # TODO: get rid of them
-  elif [[ ${image_name} == *suse* ]]
-  then
-    run_verbose zypper -q --no-gpg-checks in -y git-core curl tar gzip lsb-release binutils findutils util-linux
-    run_verbose zypper -q --no-gpg-checks in -y glibc-devel libstdc++6 # TODO: get rid of them
-  elif [[ ${image_name} == *manjaro* ]]
-  then
-    # run_verbose pacman-mirrors -g
-    run_verbose pacman -S -y -q --noconfirm
-
-    # Update even if up to date (-yy) & upgrade (-u).
-    # pacman -S -yy -u -q --noconfirm
-    run_verbose pacman -S -q --noconfirm --noprogressbar git curl tar gzip lsb-release binutils which
-    run_verbose pacman -S -q --noconfirm --noprogressbar gcc-libs # TODO: get rid of them
-  elif [[ ${image_name} == *archlinux* ]]
-  then
-    run_verbose pacman -S -y -q --noconfirm
-
-    # Update even if up to date (-yy) & upgrade (-u).
-    # pacman -S -yy -u -q --noconfirm
-    run_verbose pacman -S -q --noconfirm --noprogressbar git curl tar gzip lsb-release binutils which
-    run_verbose pacman -S -q --noconfirm --noprogressbar gcc-libs
-  fi
-
-  echo
-  echo "The system C/C++ libraries..."
-  find /usr/lib* /lib -name 'libc.*' -o -name 'libstdc++.*' -o -name 'libgcc_s.*'
 }
 
 # -----------------------------------------------------------------------------
