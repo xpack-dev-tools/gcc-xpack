f5661c1887248cdbeec966f4a38c2c1fd2dc11c0
Liviu Ionescu <ilg@livius.net>
2022-12-12 21:31:19 +0200

    tests/run.sh: add ${test_bin_path}

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -19,20 +19,22 @@
 
 function tests_run_all()
 {
+  local test_bin_path="$1"
+
   # XBB_GCC_VERSION="$(echo "${XBB_RELEASE_VERSION}" | sed -e 's|-.*||')"
   # XBB_GCC_VERSION_MAJOR=$(echo ${XBB_GCC_VERSION} | sed -e 's|\([0-9][0-9]*\)\..*|\1|')
 
   # Call the functions defined in the build code.
   if [ "${XBB_HOST_PLATFORM}" != "darwin" ]
   then
-    test_binutils
+    test_binutils "${test_bin_path}"
   fi
 
-  test_gcc
+  test_gcc "${test_bin_path}"
 
   if [ "${XBB_HOST_PLATFORM}" != "darwin" ]
   then
-    test_gdb
+    test_gdb "${test_bin_path}"
   fi
 }
 
