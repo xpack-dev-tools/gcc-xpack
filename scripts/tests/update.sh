73ebb22c1a93b5753eaf4adc286ae0b9aa106b66
Liviu Ionescu <ilg@livius.net>
2023-08-31 13:30:32 +0300

    tests/update.sh: disable 32-bit tests RedHat & Co

--- scripts/tests/update.sh
+++ scripts/tests/update.sh
@@ -16,36 +16,24 @@ function tests_update_system()
   # Make sure that the minimum prerequisites are met.
   if [[ ${image_name} == github-actions-ubuntu* ]]
   then
-    run_verbose sudo apt-get update
-    # To make 32-bit tests possible.
-    run_verbose sudo apt-get -qq install --yes g++-multilib
+    :
   elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]]
   then
-    if [ "$(uname -m)" == "x86_64" ]
-    then
-      run_verbose apt-get -qq install --yes g++ g++-multilib
-    else
-      run_verbose apt-get -qq install --yes g++
-    fi
+    :
   elif [[ ${image_name} == *raspbian* ]]
   then
-    run_verbose apt-get -qq install --yes g++
     export XBB_SKIP_32_BIT_TESTS="y"
   elif [[ ${image_name} == *centos* ]] || [[ ${image_name} == *redhat* ]] || [[ ${image_name} == *fedora* ]]
   then
-    run_verbose yum install --assumeyes --quiet gcc-c++ glibc glibc-common
     export XBB_SKIP_32_BIT_TESTS="y"
   elif [[ ${image_name} == *suse* ]]
   then
-    run_verbose zypper --quiet --no-gpg-checks install --no-confirm gcc-c++ glibc
     export XBB_SKIP_32_BIT_TESTS="y"
   elif [[ ${image_name} == *manjaro* ]]
   then
-    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc
     export XBB_SKIP_32_BIT_TESTS="y"
   elif [[ ${image_name} == *archlinux* ]]
   then
-    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc
     export XBB_SKIP_32_BIT_TESTS="y"
   else
     export XBB_SKIP_32_BIT_TESTS="y"
