bdfadee5624745ebe9196e9f04fb0eee7c35241e
Liviu Ionescu <ilg@livius.net>
2024-04-13 19:43:06 +0300

    application.sh: XBB_APPLICATION_TEST_PRERELEASE

--- scripts/application.sh
+++ scripts/application.sh
@@ -28,8 +28,11 @@ declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( libiconv zlib gmp mpfr mpc isl 
 XBB_APPLICATION_HAS_FLEX_PACKAGE="y"
 
 # XBB_APPLICATION_BOOTSTRAP_ONLY="y"
+
 # XBB_APPLICATION_HAS_LIBICONV2DYLIB="y"
-# XBB_APPLICATION_TEST_PRERELEASE="y"
+XBB_APPLICATION_HAS_LIBZ1DYLIB="y"
+
+XBB_APPLICATION_TEST_PRERELEASE="y"
 
 # -----------------------------------------------------------------------------
 
