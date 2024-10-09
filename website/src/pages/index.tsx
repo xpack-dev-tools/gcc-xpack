9c696b6ffa73e841bbe29e9db25b4b38b17a38c7
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:32:47 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the GCC executables">
+      description="A binary package with the GCC executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
