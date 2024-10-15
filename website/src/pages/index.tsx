403c134d313eef995956482c410514d8dd5652dc
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:33:50 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to gcc`}
+      title="Welcome!"
       description="A binary package with the gcc executables">
       <HomepageHeader />
       <main>
