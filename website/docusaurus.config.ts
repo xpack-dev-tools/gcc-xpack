6b7a0f4b27b74fd47504eebe05fbb0edbc0ff2bc
Liviu Ionescu <ilg@livius.net>
2025-01-10 13:20:49 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,7 +89,8 @@ console.log('customFields: ' + util.inspect(customFields));
 // ----------------------------------------------------------------------------
 
 const config: Config = {
-  title: 'xPack GCC',
+  title: 'xPack GCC' +
+    ((process.env.DOCUSAURUS_IS_PREVIEW === 'true') ? ' (preview)' : ''),
   tagline: 'A binary distribution of GCC',
   // Explicitly set in headTags.
   // favicon: '/img/favicon.ico',
@@ -260,7 +261,7 @@ const config: Config = {
       items: [
         {
           to: '/',
-          // label: 'Home',
+          label: 'gcc',
           className: 'header-home-link',
           position: 'left'
         },
