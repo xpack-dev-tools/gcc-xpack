083bf88244f322bb469eaf36f5e89bb5aaf0d22e
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:21:53 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/gcc@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/gcc@14.2.0-2.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
