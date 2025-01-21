eadadb103043459f05e787f71c56a6021e1bbddb
Liviu Ionescu <ilg@livius.net>
2025-01-21 12:14:06 +0200

    website: re-generate commons

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -275,7 +275,7 @@ const config: Config = {
           label: 'GitHub',
           items: [
             {
-              label: `gcc-xpack`,
+              label: `gcc-xpack project`,
               href: `https://github.com/xpack-dev-tools/gcc-xpack/`,
             },
             {
@@ -348,7 +348,7 @@ const config: Config = {
               href: 'https://www.paypal.com/donate/?hosted_button_id=5MFRG9ZRBETQ8',
             },
             {
-              label: 'GitHub gcc-xpack',
+              label: 'GitHub gcc-xpack project',
               href: 'https://github.com/xpack-dev-tools/gcc-xpack/',
             },
             {
