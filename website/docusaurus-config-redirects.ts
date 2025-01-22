e0dcafd646bfa29c2b0d20f0eb449b11da526a1d
Liviu Ionescu <ilg@livius.net>
2025-01-22 13:14:32 +0200

    website: re-generate commons

--- website/docusaurus-config-redirects.ts
+++ website/docusaurus-config-redirects.ts
@@ -25,7 +25,7 @@ export const redirects = {
     //     to: '/docs/newDoc2',
     //     from: ['/docs/oldDocFrom2019', '/docs/legacyDocFrom2016'],
     //   },
-    
+
     {
       from: '/docs/developer-info',
       to: '/docs/developer',
@@ -37,7 +37,11 @@ export const redirects = {
     {
       from: '/docs/user-info',
       to: '/docs/user',
-    }
+    },
+    {
+      from: '/docs/about',
+      to: '/docs/project/about',
+    },
   ],
   createRedirects(existingPath) {
     console.log(existingPath);
@@ -56,7 +60,7 @@ export const redirects = {
     //   }
     //   return undefined; // Return a falsy value: no redirect created
     //   },
-    
+
     if (existingPath.includes('/user-info')) {
       return [
         existingPath.replace('/user-info', '/user')
@@ -70,5 +74,5 @@ export const redirects = {
         existingPath.replace('/maintainer-info', '/maintainer')
       ];
     }
-}
+  }
 }
